0bbcdb7484fd58fe2b388da8bdbe86589e70d125
manikantar9527 <107861189+manikantar9527@users.noreply.github.com>
2022-12-20 16:24:04 +0530

    reservation apis added

---
+++ spring-gateway-security/src/main/java/com/persistent/security/util/AppConstants.java
@@ -0,0 +1,9 @@
+package com.persistent.security.util;
+
+public class AppConstants {
+    //service id or route id of reservation service
+    public static final String RESERVATION_SERVICE_KEY = "424C481A-DA17-4908-85B1-6740E7808B0D";
+    //service id or route id of book ticket service
+    public static final String BOOK_TICKET_SERVICE_KEY = "EE44BAD9-A3DA-46FA-B4E0-7DE7C2681ABF";
+    public static final String RECORD_KEY = "apiKeys";
+}
