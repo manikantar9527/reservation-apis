d8f91a3ba3867d30d87f99e9c64b3e1d00d7cf29
manikantar9527 <107861189+manikantar9527@users.noreply.github.com>
2022-12-22 10:46:13 +0530

    removed unused class

--- reservation-service/src/main/java/com/persistent/util/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.persistent.util;
-
-public class AppConstants {
-    public static final String STUDENT_ADDED = "Student details added successfully";
-    public static final String STUDENTS_ADDED = "Students details added successfully";
-}
-
